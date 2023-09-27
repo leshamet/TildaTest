bb70b321cf604b3dc57be462fa1a406e9b797761
leshamet <136517477+leshamet@users.noreply.github.com>
2023-09-27 18:17:00 +0300

    Update MyProject-HTML5-Shipping.data.js

--- MyProject-HTML5-Shipping.data.js
+++ MyProject-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/Unreal Engine for TILDA/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/b4l1cblb0qg7y9ujy60hs/MyProject-HTML5-Shipping.data?rlkey=jlpz9uki7jp9kf8mlw5j70pkv&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/b4l1cblb0qg7y9ujy60hs/MyProject-HTML5-Shipping.data?rlkey=jlpz9uki7jp9kf8mlw5j70pkv&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
